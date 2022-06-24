82b5072fd1f5510b42831e1a624b27ed231aa490
misky-jelle <miskyjelle@gmail.com>
2022-06-24 16:41:12 +0300

    first commit

--- practice.py
+++ practice.py
@@ -1,14 +1,24 @@
-def word(d):
-   y=d.split()
-   start = 0
-   end = len(y)-1
-   while start < end:
-       y[start],y[end]=y[end],y[start]
-       start+=1
-       end-=1
-       st=" "
-       print(st.join(y))
-word("I am Akirachix")       
+# def word(d):
+#    y=d.split()
+#    start = 0
+#    end = len(y)-1
+#    while start < end:
+#        y[start],y[end]=y[end],y[start]
+#        start+=1
+#        end-=1
+#        st=" "
+#        print(st.join(y))
+# word("I am Akirachix")       
+
+
+v="committee"
+print(v.count("m"))
+# f=[]
+# for i in v:
+#     # if i not in f:
+#     #   print( f.append(i)) 
+#     if f.count(i!=1):
+#         print( f.append(i))
 
 
 
--- questions.py
+++ questions.py
@@ -1,2 +1,10 @@
 d={1:2,3:4,5:6,7:8}
 print("original dictionary")
+
+
+x="committee"
+y=""
+for i in x:
+    if i not in y:
+        # y+=0
+        print(y)
