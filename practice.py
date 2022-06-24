b3946f23e63167a74ebc3caa5d174bc0d0f6434a
misky-jelle <miskyjelle@gmail.com>
2022-06-24 16:11:36 +0300

    change

---
+++ practice.py
@@ -0,0 +1,20 @@
+def word(d):
+   y=d.split()
+   start = 0
+   end = len(y)-1
+   while start < end:
+       y[start],y[end]=y[end],y[start]
+       start+=1
+       end-=1
+       st=" "
+       print(st.join(y))
+word("I am Akirachix")       
+
+
+
+
+
+
+    
+
+
--- strings.py
+++ strings.py
@@ -1,27 +1,39 @@
-# How to turn the first half into uppercase
-x="childrenplayinginplayground"
-y=len(x)//2
-second=x[y:].upper()
-first=x[:y].lower()
-third=first+second
-print(third)
+# # How to turn the second half into uppercase
+# x="childrenplayinginplayground"
+# y=len(x)//2
+# second=x[y:].upper()
+# first=x[:y].lower()
+# sum=first+second
+# print(sum)
+# # How to turn the first half into uppercase
+# x="childrenplayinginplayground"
+# y=len(x)//2
+# second=x[y:].lower()
+# first=x[:y].upper()
+# third=first+second
+# print(third)
+# # How to reverse and put y in upper case
+# n="childrenplayinginplayground"
+# t=len(n)//2
+# y=n[t:].upper()
+# j=n[:t].lower()
+# v=y+j
+# print(v)
 
-x="childrenplayinginplayground"
-y=len(x)//2
-second=x[y:].lower()
-first=x[:y].upper()
-third=first+second
-print(third)
+#Accessing string characters in Python
+str = 'programiz'
+print(str)
 
+#first character
+print(str[0])
 
-# How to reverse and put y in upper case
-n="childrenplayinginplayground"
-k=len(n)//2
-y=n[k:].upper()
-j=n[:k].lower()
-v=y+j
-print(v)
+#last character
+print(str[-1])
+#slicing 2nd to 5th character
+print(str[1:5])
 
+#slicing 6th to 2nd last character
+print(str[5:-2])
 
 
 
